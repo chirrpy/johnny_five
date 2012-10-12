f6be50085d810d988f48f3b67599675c28bf36e3
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-10-12 14:02:24 +0800

    Added to date_time_input.rb to get child_index to use in id

--- lib/johnny_five/date_time_input.rb
+++ lib/johnny_five/date_time_input.rb
@@ -1,5 +1,7 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input
-    %Q{<input id="#{(lookup_model_names << attribute_name).join('_')}attributes_#{options[:child_index]}_datepicker" type="text" class="datetime string datepicker">#{super}}.html_safe
+    input_value = super
+    child_index = input_value.match(/id="\w+_(\d+)_"/)[1]
+    %Q{<input id="#{(lookup_model_names << attribute_name).join('_')}_attributes_#{child_index}_datepicker" type="text" class="datetime string datepicker">#{input_value}}.html_safe
   end
 end
