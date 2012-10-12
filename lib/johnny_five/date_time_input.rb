ffbc235de493b32bce92df0175d7a8a893145198
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-10-12 13:40:19 +0800

    For date_time_input.rb added additional parameters to id to make it unique and consistent

--- lib/johnny_five/date_time_input.rb
+++ lib/johnny_five/date_time_input.rb
@@ -1,5 +1,5 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input
-    %Q{<input id="#{(lookup_model_names << attribute_name).join('_')}_datepicker" type="text" class="datetime string datepicker">#{super}}.html_safe
+    %Q{<input id="#{(lookup_model_names << attribute_name).join('_')}attributes_#{options[:child_index]}_datepicker" type="text" class="datetime string datepicker">#{super}}.html_safe
   end
 end
