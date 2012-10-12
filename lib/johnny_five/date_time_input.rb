e9f97df36083dfd295637c76e33365cccad58a61
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-10-12 14:49:53 +0800

    Modified date_time_input.rb to completely remove ID to solve issue with jquery datepicker

--- lib/johnny_five/date_time_input.rb
+++ lib/johnny_five/date_time_input.rb
@@ -1,14 +1,5 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input
-    input_value = super
-    child_index = input_value.match(/id="\w+_(\d+_\w+)/)[1] rescue nil
-
-    if child_index
-      child_index = "_attributes_#{child_index}"
-    else
-      child_index = "_#{attribute_name}"
-    end
-
-    %Q{<input id="#{(lookup_model_names).join('_')}#{child_index}_datepicker" type="text" class="datetime string datepicker">#{input_value}}.html_safe
+    %Q{<input type="text" class="datetime string datepicker">#{super}}.html_safe
   end
 end
