8148ba56bc4d6d826c909c6abe6174c6b99a0f09
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-10-13 01:31:12 +0800

    Changed date_time_input.rb to include an identifier that doesn't need to be unique as data-id

--- lib/johnny_five/date_time_input.rb
+++ lib/johnny_five/date_time_input.rb
@@ -1,5 +1,14 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input
-    %Q{<input type="text" class="datetime string datepicker">#{super}}.html_safe
+    input_value = super
+    child_index = input_value.match(/id="\w+_(\d+_\w+i)/)[1] rescue nil
+
+    if child_index
+      child_index = "_attributes_#{child_index}"
+    else
+      child_index = "_#{attribute_name}"
+    end
+
+    %Q{<input data-id="#{(lookup_model_names).join('_')}#{child_index}_datepicker" type="text" class="datetime string datepicker">#{input_value}}.html_safe
   end
 end
